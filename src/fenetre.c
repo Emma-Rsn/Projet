f24b84ac502e8be5dfb100991a11ff6d5030fbc3
Lina <Lina.pasquier.Etu@univ-lemans.fr>
2024-01-26 13:23:52 +0100

    fenetre doxygen

--- src/fenetre.c
+++ src/fenetre.c
@@ -1,13 +1,25 @@
+//
+//  fenetre.c
+//
+//  Created by Moreau Enzo on 26/01/2024.
 /**
-* fenetre.c
-* 
-*
+ *  \file fenetre.c
+ * \brief Programme qui initialise la fenetre SDL et son rendu
+ * \author Enzo Moreau
+ * \version 1.0
+ * \date 26 janvier 2024
 */
+
+
 
 
 #include <SDL2/SDL.h>
 
-
+/**
+ * \fn int init_window (SDL_Window *window,SDL_Renderer *renderer)
+ * \brief Programme qui initialise la fenetre SDL et son rendu
+ * \param *window pointeur sur fenetre (type de SDL) *renderer pointeur sur rendu (type de SDL) 
+*/
 int init_window(SDL_Window *window,SDL_Renderer *renderer){
     // Initialisation de SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
