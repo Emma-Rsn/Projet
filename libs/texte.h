72f2a20b7cbcc4079903c26dcfb3f71269e21eee
lina <lina.pasquier.etu@univ-lemans.fr>
2024-01-28 13:17:20 +0100

    dialogue

--- libs/texte.h
+++ libs/texte.h
@@ -1,7 +1,9 @@
 #ifndef __TEXTE_H__
 #define __TEXTE_H__
 
+extern SDL_Event event;
+int affiche_texte(SDL_Renderer * rendu,char *mess,int dim,SDL_Color color);
+void dialogue (SDL_Renderer * rendu,int dim);
 
-int affiche_texte(void);
 
 #endif
--- src/TLN.c
+++ src/TLN.c
@@ -1,21 +1,27 @@
 #include <SDL2/SDL.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <SDL2/SDL_ttf.h>
 
 #include "../libs/truc.h"
 #include "../libs/Pmov.h"
 #include "../libs/texte.h"
 #include "../libs/printImg.h"
+#include "../libs/texte.h"
 
 int main(){
+
+
      // Initialisation de SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "Erreur d'initialisation de SDL : %s\n", SDL_GetError());
         return -1;
     }
+    
+    int dim=480;
+    
     // Création de la fenêtre
-    SDL_Window *window = SDL_CreateWindow("The Last Nightmare", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow("The Last Nightmare", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, dim, SDL_WINDOW_SHOWN);
     if (window == NULL) {
         fprintf(stderr, "Erreur de création de la fenêtre : %s\n", SDL_GetError());
         SDL_Quit();
@@ -30,10 +36,13 @@ int main(){
         SDL_Quit();
         return -1;
     }
-
-    affiche_texte();
+    
+	
+    	
+	
 
 	test();
+
 
 	point_mv point;
 	point = initpoint();
@@ -45,11 +54,13 @@ int main(){
             if (event.type == SDL_QUIT) {
                 run = 0;
             }
+            dialogue(renderer,dim);
+            
             pinput(ppoint);
         }
 
         // Affiche le fond d'ecran
-        print_bg(window,renderer);
+        //print_bg(window,renderer);
         
         // Dessiner un cadre
         SDL_SetRenderDrawColor(renderer, 0, 50, 200, 255);
