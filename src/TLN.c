308da9d1350f60203ba71d488e5569af5afc013b
Emma.Rsn <rasson.emma@gmail.com>
2024-03-13 22:03:54 +0100

    ajout texture arbres+rocher+barre de vie

--- src/TLN.c
+++ src/TLN.c
@@ -204,7 +204,7 @@ int main(){
         //Affiche un personnage
         affp(pAlex,renderer);
 
-        //lumiere(renderer,cartec,pAlex->c);
+        lumiere(renderer,cartec,pAlex->c);
 
         //afficher dialogue
         pnj_dialogue (event,pAlex2,renderer,hEcran,wEcran);
