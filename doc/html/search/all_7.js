aa578ee78412b54b5b4816daeb95c5495242dec0
Emma-Rsn <Emma.Rasson.Etu@univ-lemans.fr>
2024-04-22 15:30:25 +0200

    maj readme

--- doc/html/search/all_7.js
+++ doc/html/search/all_7.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['init_5fartefact_0',['init_artefact',['../OBJ_8c.html#a7db3392d374e07d5c5a4420c5e119a4c',1,'OBJ.c']]],
   ['init_5fcombat_1',['init_combat',['../combat_8c.html#a4c96e4b113cdef70898987ec2643b58a',1,'combat.c']]],
-  ['inventaire_2',['inventaire',['../menu_8c.html#aa0dea1e510b75502550a28de7982a1b4',1,'menu.c']]]
+  ['initp_2',['initp',['../Pmov_8c.html#ab233e299850dfbf68e32adb1e8684a6c',1,'Pmov.c']]],
+  ['inventaire_3',['inventaire',['../menu_8c.html#aa0dea1e510b75502550a28de7982a1b4',1,'menu.c']]]
 ];
